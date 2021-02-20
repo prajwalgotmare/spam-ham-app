b5b02b3912f5dfade5c561709b2fc4f05b3119a8
prajwal gotmare <prajwalvgotmare@gmail.com>
2021-02-20 20:33:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import pickle
+
+# load the model from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv = pickle.load(open('transform.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html', prediction = my_prediction)
+
+if __name__ == '__main__':
+	app.run()
